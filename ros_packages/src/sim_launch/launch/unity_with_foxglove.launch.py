f8003fe32b9a164910d2a2ca481d3d69f96aba0b
alray <1780284652@qq.com>
2026-01-24 08:23:39 +0800

    [feat] 添加UnityRosEndpoint支持和(Foxglove + UnityTcpEndpoint)RosLaunch

---
+++ ros_packages/src/sim_launch/launch/unity_with_foxglove.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    # Unity TCP Endpoint（ROS–Unity 通信）
+    unity_endpoint = Node(
+        package="ros_tcp_endpoint",
+        executable="default_server_endpoint",
+        name="unity_tcp_endpoint",
+        output="screen",
+        parameters=[{
+            "ROS_IP": "0.0.0.0",     # 监听所有地址
+            "ROS_TCP_PORT": 40425    # Unity 默认端口
+        }]
+    )
+
+    # Foxglove Bridge
+    foxglove = Node(
+        package="foxglove_bridge",
+        executable="foxglove_bridge",
+        name="foxglove_bridge",
+        output="screen",
+        parameters=[{
+            "port": 8765,
+            "address": "0.0.0.0"
+        }]
+    )
+
+    return LaunchDescription([
+        unity_endpoint,
+        foxglove
+    ])
